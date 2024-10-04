e193905a58168da1c80f5e2a0077355b530ef1d8
hwabis <kennysun23@gmail.com>
2024-10-04 16:48:04 -0400

    ok i think the inputs work now

--- osu.Game.Rulesets.PumpTrainer/Objects/Drawables/DrawablePumpTrainerHitObject.cs
+++ osu.Game.Rulesets.PumpTrainer/Objects/Drawables/DrawablePumpTrainerHitObject.cs
@@ -1,17 +1,35 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Textures;
+using osu.Framework.Input.Bindings;
+using osu.Framework.Input.Events;
 using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Scoring;
 using osuTK;
 
 namespace osu.Game.Rulesets.PumpTrainer.Objects.Drawables
 {
-    public partial class DrawablePumpTrainerHitObject : DrawableHitObject<PumpTrainerHitObject>
+    public partial class DrawablePumpTrainerHitObject : DrawableHitObject<PumpTrainerHitObject>, IKeyBindingHandler<PumpTrainerAction>
     {
         public const int WIDTH = 85;
+
+        private static Dictionary<PumpTrainerAction, Column> actionToColumn = new()
+        {
+            { PumpTrainerAction.P1DL, Column.P1DL },
+            { PumpTrainerAction.P1UL, Column.P1UL },
+            { PumpTrainerAction.P1C, Column.P1C },
+            { PumpTrainerAction.P1UR, Column.P1UR },
+            { PumpTrainerAction.P1DR, Column.P1DR },
+            { PumpTrainerAction.P2DL, Column.P2DL },
+            { PumpTrainerAction.P2UL, Column.P2UL },
+            { PumpTrainerAction.P2C, Column.P2C },
+            { PumpTrainerAction.P2UR, Column.P2UR },
+            { PumpTrainerAction.P2DR, Column.P2DR },
+        };
 
         public DrawablePumpTrainerHitObject(PumpTrainerHitObject hitObject)
             : base(hitObject)
@@ -32,9 +50,22 @@ namespace osu.Game.Rulesets.PumpTrainer.Objects.Drawables
 
         protected override void CheckForResult(bool userTriggered, double timeOffset)
         {
-            if (timeOffset >= 0)
-                // todo: implement judgement logic
-                ApplyMaxResult();
+            if (!userTriggered)
+            {
+                if (!HitObject.HitWindows.CanBeHit(timeOffset))
+                {
+                    ApplyMinResult();
+                }
+
+                return;
+            }
+
+            var result = HitObject.HitWindows.ResultFor(timeOffset);
+
+            if (result == HitResult.None)
+                return;
+
+            ApplyResult(result);
         }
 
         protected override void UpdateHitStateTransforms(ArmedState state)
@@ -49,6 +80,20 @@ namespace osu.Game.Rulesets.PumpTrainer.Objects.Drawables
                     // todo ?
                     break;
             }
+        }
+
+        public bool OnPressed(KeyBindingPressEvent<PumpTrainerAction> e)
+        {
+            if (actionToColumn[e.Action] == HitObject.Column)
+            {
+                return UpdateResult(true);
+            }
+
+            return false;
+        }
+
+        public void OnReleased(KeyBindingReleaseEvent<PumpTrainerAction> e)
+        {
         }
     }
 }
--- osu.Game.Rulesets.PumpTrainer/PumpTrainerRuleset.cs
+++ osu.Game.Rulesets.PumpTrainer/PumpTrainerRuleset.cs
@@ -20,7 +20,7 @@ namespace osu.Game.Rulesets.PumpTrainer
 {
     public class PumpTrainerRuleset : Ruleset
     {
-        public override string Description => "a very pumptrainer ruleset";
+        public override string Description => "Pump It Up!";
 
         public override DrawableRuleset CreateDrawableRulesetWith(IBeatmap beatmap, IReadOnlyList<Mod> mods = null) => new DrawablePumpTrainerRuleset(this, beatmap, mods);
 
@@ -80,15 +80,15 @@ namespace osu.Game.Rulesets.PumpTrainer
         public override IEnumerable<KeyBinding> GetDefaultKeyBindings(int variant = 0) => new[]
         {
             new KeyBinding(InputKey.Z, PumpTrainerAction.P1DL),
-            new KeyBinding(InputKey.W, PumpTrainerAction.P1UL),
+            new KeyBinding(InputKey.Q, PumpTrainerAction.P1UL),
             new KeyBinding(InputKey.S, PumpTrainerAction.P1C),
             new KeyBinding(InputKey.E, PumpTrainerAction.P1UR),
-            new KeyBinding(InputKey.X, PumpTrainerAction.P1DR),
-            new KeyBinding(InputKey.C, PumpTrainerAction.P2DL),
+            new KeyBinding(InputKey.C, PumpTrainerAction.P1DR),
+            new KeyBinding(InputKey.V, PumpTrainerAction.P2DL),
             new KeyBinding(InputKey.R, PumpTrainerAction.P2UL),
-            new KeyBinding(InputKey.F, PumpTrainerAction.P2C),
-            new KeyBinding(InputKey.T, PumpTrainerAction.P2UR),
-            new KeyBinding(InputKey.V, PumpTrainerAction.P2DR),
+            new KeyBinding(InputKey.G, PumpTrainerAction.P2C),
+            new KeyBinding(InputKey.Y, PumpTrainerAction.P2UR),
+            new KeyBinding(InputKey.N, PumpTrainerAction.P2DR),
         };
 
         // TODO make an actual icon like an arrow or something.
