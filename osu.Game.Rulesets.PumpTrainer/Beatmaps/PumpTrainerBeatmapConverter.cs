70246eb3a1d57085edebb6856000d8ec5079cf97
hwabis <kennysun23@gmail.com>
2024-10-08 22:01:47 -0400

    now you don't have to be afraid of maps with buzz sliders 😊

--- osu.Game.Rulesets.PumpTrainer/Beatmaps/PumpTrainerBeatmapConverter.cs
+++ osu.Game.Rulesets.PumpTrainer/Beatmaps/PumpTrainerBeatmapConverter.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.PumpTrainer.Objects;
@@ -39,6 +40,16 @@ namespace osu.Game.Rulesets.PumpTrainer.Beatmaps
 
                     int hitObjectsToReturnAfterFirst = hasRepeats.RepeatCount + 1; // +1 for the last hit object
                     double durationBetweenPoints = (hasRepeats.EndTime - original.StartTime) / hitObjectsToReturnAfterFirst;
+
+                    // Buzz slider protection!
+                    // If the duration between the points is too small (e.g. the beatmap has a 1/6 or 1/8 buzz slider instead of the typical 1/4 (blue tick)),
+                    // fill the duration with notes 1/4 apart instead of with the original rhythm.
+                    TimingControlPoint currentTimingPoint = beatmap.ControlPointInfo.TimingPointAt(original.StartTime);
+
+                    if (durationBetweenPoints < currentTimingPoint.BeatLength / 4)
+                    {
+                        durationBetweenPoints = currentTimingPoint.BeatLength / 4;
+                    }
 
                     const double rounding_error = 5;
 
